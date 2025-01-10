38132f024cf9bc913a95453192c65132dad796ac
alexcgar <5568981@alu.murciaeduca.es>
2025-01-10 11:09:12 +0100

    Building the frontend

--- backend/run.py
+++
@@ -1,9 +0,0 @@
-from app import create_app  # Importamos la función create_app
-from waitress import serve  # Importamos el servidor Waitress
-
-# Creamos la aplicación Flask
-app = create_app()
-
-if __name__ == '__main__':
-    # Ejecutamos la aplicación con Waitress en modo producción
-    serve(app, host='0.0.0.0', port=5000)
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Asegura rutas relativas para archivos estáticos
+  build: {
+    outDir: 'dist', // Directorio de salida
+    assetsDir: 'assets', // Carpeta para los recursos estáticos
+  },
 })
